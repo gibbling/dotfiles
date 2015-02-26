f45fa01e71464b29518624ecc2493d65e6d5cf33
Gibbling <bryan.kurpiel@mckesson.com>
2015-02-26 09:28:32 -0500

    changes to fish and bash profile. environment vars set

--- bash_profile
+++ bash_profile
@@ -15,7 +15,7 @@ alias homebrew='ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/g
 # Env Vars
 export HOMEBREW_GITHUB_API_TOKEN=930d18c99e13c1180ef83fd5961d87eedf55a6a1
 
-export PATH=/usr/local/bin:$PATH
+#export PATH='/usr/local/bin:$PATH'
 
 # Bash-Completion
 if [ -f `brew --prefix`/etc/bash_completion ]; then
--- fish/config.fish
+++ fish/config.fish
@@ -1,11 +1,27 @@
 # Path to your oh-my-fish.
 set fish_path $HOME/.dotfiles/oh-my-fish
 
+# Exports
+set -U -x ORACLE_HOME /Applications/oracle/product/instantclient_64/11.2.0.4.0
+set -U -x DYLD_LIBRARY_PATH $ORACLE_HOME/lib
+set -U -x EDITOR vim
+
+# PATH
+set -gx PATH /opt/chefdk/bin $PATH
+set -gx PATH $PATH $ORACLE_HOME/bin
+
 # Theme
 set fish_theme agnoster
 
-set -g theme_display_user yes
-set -g default_user nergal
+# Welcome Message
+set -U -x fish_greeting ''
+
+# CHEF
+set -U -x OPSCODE_USER enev7ow
+set -U -x ORGNAME mhsit
+
+set -U theme_display_user yes
+set -U default_user nergal
 
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-fish/plugins/*)
 # Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
