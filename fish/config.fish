11ea1b7006ae47dc7dee9dcca1bf7eb48527569c
Gibbling <bryan.kurpiel@mckesson.com>
2015-08-07 13:41:23 -0400

    update to config.fish and addition of fishmarks as submodule

--- fish/config.fish
+++ fish/config.fish
@@ -5,16 +5,51 @@ set fish_path $HOME/.dotfiles/oh-my-fish
 set -U -x ORACLE_HOME /Applications/oracle/product/instantclient_64/11.2.0.4.0
 set -U -x DYLD_LIBRARY_PATH $ORACLE_HOME/lib
 set -U -x EDITOR vim
+set -U -x HOMEBREW_GITHUB_API_TOKEN 930d18c99e13c1180ef83fd5961d87eedf55a6a1
 
 # PATH
 set -gx PATH /opt/chefdk/bin $PATH
-set -gx PATH $PATH $ORACLE_HOME/bin
+set -gx PATH $ORACLE_HOME/bin $PATH 
 
 # Theme
 set fish_theme agnoster
 
+# Dircolors
+# ~/.dircolors/themefile
+if test -e ~/.dir_colors
+    switch (uname)
+        case Darwin
+            eval (gdircolors -c ~/.dircolors/dircolors.256dark)
+
+        case Linux
+            eval (dircolors -c ~/.dircolors/dircolors.256dark)
+    end
+end
+
+# Shortcuts
+switch (uname)
+    case Darwin
+        alias ls 'gls --color=auto -F'
+        alias sort 'gsort'
+        alias sublime 'open -a Sublime\ Text.app'
+
+    case Linux
+        alias ls 'ls --color=auto -F'
+end
+alias grep 'grep --color=auto'
+alias less 'less -R'
+alias .. 'cd ..'
+alias remake "make clean; and make -j2"
+
 # Welcome Message
 set -U -x fish_greeting ''
+
+# Load fishmarks (http://github.com/techwizrd/fishmarks)
+if test -e $HOME/.dotfiles/fish/.fishmarks/marks.fish
+    . $HOME/.dotfiles/fish/.fishmarks/marks.fish
+end
+
+
 
 # CHEF
 set -U -x OPSCODE_USER enev7ow
